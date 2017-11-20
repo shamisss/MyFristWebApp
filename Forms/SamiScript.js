a6e116c5556b107eba767cd190861f990140103a
shamisss <31880115+shamisss@users.noreply.github.com>
2017-11-20 23:10:13 +0400

    Update SamiScript.js

--- Forms/SamiScript.js
+++ Forms/SamiScript.js
@@ -1,18 +1,45 @@
-
 var MyData = [];
 var btnSave;
 window.onload = function () {
     btnSave = document.getElementById('btnSave');
-    btnSave.addEventListener('click', manipulateData)
+    btnClear = document.getElementById('btnClear');
+    btnSave.addEventListener('click', manipulateData);
+    btnClear.addEventListener('click', clearForm);
 };
 
 function manipulateData() {
     if (validateData()) {
-        saveData();
+        if (IsEditMode() == false) {
+            saveData();
+        }
+        else {
+            var result = updateData();
+            if (result == false) {
+                alert('wrongly updated!');
+            }
+        }
         clearForm();
         refereshResultTable();
     }
 }
+
+function IsEditMode() {
+    if (document.getElementById("hId").value == "") {
+        return false;
+    }
+    return true;
+}
+
+function clearForm() {
+    document.getElementById("hId").value = "";
+    document.getElementById("txtName").value = "";
+    document.getElementById("txtAge").value = "";
+    document.getElementById("selGender").value = "0";
+    document.getElementById("taRemarks").value = "";
+    document.getElementById("btnClear").value = "Clear";
+    document.getElementById("btnSave").value = "Save";
+}
+
 function validateData() {
     var name = document.getElementById("txtName").value;
     var age = document.getElementById("txtAge").value;
@@ -37,28 +64,46 @@ function validateData() {
 }
 
 function saveData() {
+
     var saveData = getEmptyDataObject();
+    saveData.id = getNewId();
     saveData.name = document.getElementById("txtName").value;
     saveData.age = document.getElementById("txtAge").value;
     saveData.gender = document.getElementById("selGender").value;
     saveData.remarks = document.getElementById("taRemarks").value;
     MyData.push(saveData);
 }
-function clearForm() {
-    document.getElementById("txtName").value = '';
-    document.getElementById("txtAge").value = '';
-    document.getElementById("selGender").value = '0';
-    document.getElementById("taRemarks").value = '';
+
+function updateData() {
+    var editId = document.getElementById("hId").value;
+    for (var i = 0; i < MyData.length; i++) {
+        if (MyData[i].id == editId) {
+            MyData[i].name = document.getElementById("txtName").value;
+            MyData[i].age = document.getElementById("txtAge").value;
+            MyData[i].gender = document.getElementById("selGender").value;
+            MyData[i].remarks = document.getElementById("taRemarks").value;
+            return true;
+        }
+    }
+    return false;
 }
+
 
 function getEmptyDataObject() {
     var emptyData = {
+        id: null,
         name: '',
         age: 0,
         gender: '',
         remarks: ''
     };
     return emptyData;
+}
+
+function getNewId() {
+    var d = new Date();
+    var retId = d.getFullYear() + '' + (d.getMonth() + 1) + '' + d.getDate() + '' + d.getTime();
+    return retId;
 }
 
 function refereshResultTable() {
@@ -95,7 +140,7 @@ function refereshResultTable() {
             trTmp.appendChild(tdRemarks);
             //
             var tdAction = document.createElement('td');
-            tdAction.innerHTML = '<a href="#">Edit</a>&nbsp;<a href="#">Delete</a>';
+            tdAction.innerHTML = '<a href="#" onclick="editEmp(' + MyData[i].id + ')">Edit</a>&nbsp;<a href="#" onclick="deleteEmp(' + MyData[i].id + ')">Delete</a>';
             trTmp.appendChild(tdAction);
             //
             tblResultBody.appendChild(trTmp);
@@ -105,5 +150,43 @@ function refereshResultTable() {
     }
     else {
         divResult.style.display = 'none';
+    }
+}
+
+function getEmpDataById(id) {
+    for (var i = 0; i < MyData.length; i++) {
+        if (MyData[i].id == id) {
+            return MyData[i];
+        }
+    }
+    return null;
+}
+
+function editEmp(id) {
+    var editedData = getEmpDataById(id);
+    if (editedData) {
+        document.getElementById("hId").value = editedData.id;
+        document.getElementById("txtName").value = editedData.name;
+        document.getElementById("txtAge").value = editedData.age;
+        document.getElementById("selGender").value = editedData.gender;
+        document.getElementById("taRemarks").value = editedData.remarks;
+        document.getElementById("btnClear").value = "Cancel";
+        document.getElementById("btnSave").value = "Update";
+    }
+    else {
+        alert('invalid edit request!');
+    }
+}
+
+function deleteEmp(id) {
+    if (confirm('Are you sure want tp delete this?')) {
+        for(var i=0;i<MyData.length;i++){
+            if(MyData[i].id==id){
+                MyData.pop(MyData[i]);
+                clearForm();
+                refereshResultTable();
+                return true;
+            }
+        }
     }
 }
